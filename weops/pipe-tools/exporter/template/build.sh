252e9d2aba31e471361d9be229cb28548eef1a3e
zhongming Fan <zhongmingfan@canway.net>
2023-08-07 11:55:55 +0800

    dp: exporter

---
+++ weops/pipe-tools/exporter/template/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for version in v1-8 v1-9 v1-10 v1-12 v1-14 v1-16 v1-17 v1-18 v1-19 v1-20 v1-21 v1-22 v1-23 v1-24 v1-25; do
+  standalone_output_file="standalone_${version}.yaml"
+  sed "s/{{VERSION}}/${version}/g;" standalone.tpl > ../standalone/${standalone_output_file}
+done
