9f44b6979518692fe540c420e04edcd7f1d4e065
zhongming Fan <zhongmingfan@canway.net>
2023-08-07 10:15:53 +0800

    dp: old version

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # 部署监控对象
 object=nginx
-object_versions=("1.25.1" "1.24" "1.23" "1.22" "1.21" "1.20" "1.19" "1.18" "1.17" "1.16" "1.14" "1.12" "1.10.2-r4" "1.9.15-1
-" "1.8.0-4")
+object_versions=("1.25.1" "1.24" "1.23" "1.22" "1.21" "1.20" "1.19" "1.18" "1.17" "1.16" "1.14" "1.12")
 
 value_file="bitnami_values.yaml"
 
@@ -12,7 +11,7 @@ port=30080
 
 for version in "${object_versions[@]}"; do
     version_suffix="v$(echo "$version" | grep -Eo '[0-9]{1,2}\.[0-9]{1,2}' | tr '.' '-')"
-    helm install $object-standalone-$version_suffix --namespace $object --create-namespace -f ./values/$value_file ./$object \
+    helm install $object-$version_suffix --namespace $object --create-namespace -f ./values/$value_file ./$object \
     --set image.tag=$version \
     --set commonLabels.object_version=$version_suffix \
     --set service.nodePorts.http=$port
